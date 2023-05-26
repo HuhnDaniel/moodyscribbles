97aa363b36ca29b3045d34aae4d75de67b44c1ce
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2023-05-25 19:47:57 -0700

    Implement carousel with placeholder images using Swiper

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-import Carousel from '../components/Carousel.vue'
 import PageLinks from '../components/PageLinks.vue'
+
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination } from 'swiper'
+
+const images = [
+    "https://res.cloudinary.com/cornerstone-collaborative/image/upload/v1654040935/cld-sample-2.jpg",
+    "https://res.cloudinary.com/cornerstone-collaborative/image/upload/v1654040868/sample.jpg"
+]
+
+import 'swiper/css'
+import 'swiper/css/pagination'
 </script>
 
 <template>
     <main>
-        <Carousel />
+        <div class="w-2/3 mx-auto my-8">
+            <Swiper :pagination="true" :modules="[Pagination]">
+                <SwiperSlide v-for="image in images">
+                    <img  class="w-full h-full" :src="`${image}`" />
+                </SwiperSlide>
+            </Swiper>
+        </div>
+
         <PageLinks />
     </main>
 </template>
